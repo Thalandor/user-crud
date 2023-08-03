66746fe0e1fa6d96ffd7ceba39fa7d8c60f99699
IvanBV <3891584+Thalandor@users.noreply.github.com>
2023-08-03 20:33:47 +0200

    Add minimal db config

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,31 @@
 import express from "express";
 import { router } from "./routes";
+import pgPromise from "pg-promise";
+import dotenv from "dotenv";
 
-const app = express();
-const port = 3000;
-app.use("/", router);
+dotenv.config();
 
-app.listen(port, () => {
-  return console.log(
-    `Express server is listening at http://localhost:${port} 🚀`
-  );
-});
+const start = async () => {
+  const app = express();
+  const port = 3000;
+
+  const pgp = pgPromise();
+  const dbConfig = {
+    host: process.env.DB_HOST || "",
+    port: parseInt(process.env.DB_PORT, 10) || 5432,
+    database: process.env.DB_NAME || "",
+    user: process.env.DB_USER || "",
+    password: process.env.DB_PASSWORD || "",
+  };
+  const db = pgp(dbConfig);
+
+  app.use("/", router);
+
+  app.listen(port, () => {
+    return console.log(
+      `Express server is listening at http://localhost:${port} 🚀`
+    );
+  });
+};
+
+start();
